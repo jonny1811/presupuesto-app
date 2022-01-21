834ea1796f66d025cb9794a3fe32cc1d33fa8922
jonny1811 <jonny@quanto.la>
2022-01-21 14:20:32 -0300

    feat: add ultimate changes visual into form component

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Egreso } from '../egreso/egreso.model';
+import { EgresoService } from '../egreso/egreso.service';
+import { Ingreso } from '../ingreso/ingreso.model';
+import { IngresoService } from '../ingreso/ingreso.service';
 
 @Component({
-  selector: 'app-formulario',
-  templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+	selector: 'app-formulario',
+	templateUrl: './formulario.component.html',
+	styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
 
-  constructor() { }
+	type: string = 'ingresoOperation';
+	descriptionInput: string;
+	valueInput: number;
 
-  ngOnInit(): void {
-  }
+	constructor(
+		private ingresoService: IngresoService,
+		private egresoService: EgresoService
+	) { }
+
+	ngOnInit(): void {
+	}
+
+	operationType(event): void {
+		this.type = event.target.value;
+	}
+
+	addValue(): void {
+		if (this.type === 'ingresoOperation') {
+			this.ingresoService.ingresos.push(new Ingreso(this.descriptionInput, this.valueInput));
+		} else {
+			this.egresoService.egresos.push(new Egreso(this.descriptionInput, this.valueInput));
+		}
+	}
 
 }
